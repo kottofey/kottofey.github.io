fa5e970bbf73ce900680e88b261213add6d18a39
Kottofey (BigBrother) <romeokontakt@gmail.com>
2024-04-25 18:25:42 +0300

    Swiper slider

---
+++ Experiments/swiperInit.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.brands-repair-list--wide-screen', {
+    direction: 'horizontal',
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    spaceBetween: 16,
+    slidesPerView: 1.31,
+    slidesOffsetAfter: 80,
+});
