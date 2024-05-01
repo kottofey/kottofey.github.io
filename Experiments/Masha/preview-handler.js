20dde8a2bb87413ca900a9252c7edfb3c748f5fc
kottofey (KotBookPro) <romeokontakt@gmail.com>
2024-05-01 21:36:36 +0300

    Рефакторинг и перевод на другую систему вывода картинок
    В начале скрипта создан массив-база объектов, описывающих фотки. Из него формируется панель с превьюшками. При этом каждой превью картинке вешается айди, соответствующий элементу из массива-базы.
    
    По этим же данным вешается обработчик событий на каждую превьюшку. При клике берется айди превью и по ней ищется и меняется сорс в большую картинку, а также меняется описание.
    
    При заполнении панели с превью если неправильно указан тип, то подгружается заглушка, ибо при клике на неправильно обработанную превью ничего не произойдёт.

--- Experiments/Masha/preview-handler.js
+++ Experiments/Masha/preview-handler.js
@@ -1,70 +1,114 @@
-let descriptions = [
-    "Милашная Мяу",
-    "Блестящее кошачье чудо",
-    "Знакомьтесь, это - кошечка Мурка!",
-    "Наш кот — вампир?",
-    "Апрель апрель звенит капель!",
-    "Сердце Чебурашки",
-    "Когда захотелось снять про Чебурашку на радужном фоне",
-    "Гарри Поттер",
-    "Гарри Коттер и кубок вкусняшек",
-    "Гарри Коттер и колбасный камень",
-    "Гарри Коттер и орден Котика",
-];
+const thumbnails =
+	{
+		"item1": {
+			type: 'img',
+			url: '../../imgs/Masha/1.jpeg',
+			desc: 'Милашная Мяу'
+		},
+		"item2": {
+			type: 'img',
+			url: '../../imgs/Masha/2.jpeg',
+			desc: 'Блестящее кошачье чудо'
+		},
+		"item3": {
+			type: 'img',
+			url: '../../imgs/Masha/3.jpeg',
+			desc: 'Знакомьтесь, это - кошечка Мурка!'
+		},
+		"item4": {
+			type: 'img',
+			url: '../../imgs/Masha/4.jpeg',
+			desc: 'Наш кот — вампир?'
+		},
+		"item5": {
+			type: 'vid',
+			url: '../../imgs/Masha/5.mp4',
+			desc: 'Апрель апрель звенит капель!'
+		},
+		"item6": {
+			type: 'vid',
+			url: '../../imgs/Masha/6.mp4',
+			desc: 'Сердце Чебурашки'
+		},
+		"item7": {
+			type: 'vid',
+			url: '../../imgs/Masha/7.mp4',
+			desc: 'Когда захотелось снять про Чебурашку на радужном фоне'
+		},
+		"item8": {
+			type: 'img',
+			url: '../../imgs/Masha/8.png',
+			desc: 'Гарри Поттер'
+		},
+		"item9": {
+			type: 'img',
+			url: '../../imgs/Masha/9.jpeg',
+			desc: 'Гарри Коттер и кубок вкусняшек'
+		},
+		"item10": {
+			type: 'img',
+			url: '../../imgs/Masha/10.jpg',
+			desc: 'Гарри Коттер и колбасный камень'
+		},
+		"item11": {
+			type: 'img',
+			url: '../../imgs/Masha/11.jpg',
+			desc: 'Гарри Коттер и орден Котика'
+		},
+	}
 
-let previews = document.querySelectorAll(".album__image-preview");
-let photoDescription = document.querySelector(".album__description");
-let album = document.querySelector('.album');
+// creating preview panel
+const thumbnailsPanel = document.querySelector('.album__thumbnails-panel');
+const imgTemplate = document.querySelector('#imgPreview').content;
+const vidTemplate = document.querySelector('#vidPreview').content;
+let thumbSrc;
 
+Object.keys(thumbnails).forEach(function (key) {
+	let newThumb;
 
-//Собираем превьюшки
-let previewURLs = {};
-for (let i = 0; i < previews.length; i++) {
-    previewURLs[i] = previews[i].querySelector("img, source").src;
-}
+	if (thumbnails[key].type === "img") {
+		newThumb = imgTemplate.cloneNode(true);
+	} else if (thumbnails[key].type === "vid") {
+		newThumb = vidTemplate.cloneNode(true);
+	} else {
+		console.log(">>> " + thumbnails[key].type + ": Wrong type for id '" + thumbnails[key].id + "'. Must be either 'img' or 'vid'");
+		newThumb = imgTemplate.cloneNode(true);
+		newThumb.querySelector('.album__thumbnail-source').src = 'https://fakeimg.pl/350x200/?text=N/A&font=lobster';
+		thumbnailsPanel.appendChild(newThumb);
 
-let createClickHandler = function (preview, url, description) {
-    let newTag;
-    let newTagSrc;
+		return;
+	}
 
-    preview.addEventListener('click', function () {
-        let bigPicture = document.querySelector(".album__big-picture");
+	thumbSrc = newThumb.querySelector('.album__thumbnail-source');
+	thumbSrc.src = thumbnails[key].url;
+	newThumb.querySelector('.album__thumbnail').id = key;
+	thumbnailsPanel.appendChild(newThumb);
+});
 
-        console.log(bigPicture.tagName);
+// Looking for all thumbs
+const allThumbs = document.querySelectorAll('.album__thumbnail-wrapper');
 
-        // готовим теги для большой картинки и видоса
-        if (preview.children[0].tagName === "IMG") {
+allThumbs.forEach(function (thumb) {
+	const id = thumb.querySelector('.album__thumbnail').id;
+	const src = thumbnails[id].url;
+	const type = thumbnails[id].type;
+	const desc = thumbnails[id].desc;
+	const bigPicWrapper = document.querySelector('.album__fullsize-img-wrapper');
+	const bigVidWrapper = document.querySelector('.album__fullsize-vid-wrapper');
+	let bigPicSrc = document.querySelector('.album__big-pic-source');
+	let bigVidSrc = document.querySelector('.album__big-vid-source');
+	let bigPicDesc = document.querySelector('.album__fullsize-description');
 
-            newTag = document.createElement("img");
-            newTag.classList.add('album__big-picture');
-            newTag.src = url;
-
-            album.replaceChild(newTag, bigPicture);
-        } else {
-            newTag = document.createElement("video");
-            newTag.classList.add('album__big-picture');
-            newTagSrc = document.createElement("source");
-            newTagSrc.type = "video/mp4";
-            newTagSrc.src = url;
-
-            newTag.loop = true;
-            newTag.playsInline = true;
-            newTag.muted = true;
-            newTag.preload = "metadata";
-            newTag.autoplay = true;
-            newTag.append(newTagSrc);
-
-            // console.log(bigPicture);
-            // console.log(newTag);
-
-            album.replaceChild(newTag, bigPicture);
-        }
-
-        photoDescription.textContent = description;
-    });
-};
-
-for (let i = 0; i < previews.length; i++) {
-    createClickHandler(previews[i], previewURLs[i], descriptions[i]);
-    // console.log(previews[i].querySelector("source, img").tagName);
-}
+	thumb.addEventListener('click', function () {
+		if (type === "img") {
+			bigPicSrc.src = src;
+			bigPicWrapper.classList.remove('album__fullsize-wrapper--hidden');
+			bigVidWrapper.classList.add('album__fullsize-wrapper--hidden');
+		} else if (type === "vid") {
+			bigVidSrc.src = src;
+			bigVidWrapper.classList.remove('album__fullsize-wrapper--hidden');
+			bigPicWrapper.classList.add('album__fullsize-wrapper--hidden');
+		}
+		bigPicDesc.textContent = desc;
+	});
+});
